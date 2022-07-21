94f4e020aa61c7b2495383bad601e947d09ea9db
Ato11 <72277559+Ato11@users.noreply.github.com>
2022-07-21 09:23:33 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -4,42 +4,36 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<script src="jquery.min.js"></script>
+	<script src="supervisor/venta/js/jquery.min.js"></script>
 	<title>Reinas</title>
 </head>
 <body>
 	Cantidad
 	<input type="text" id="numero" placeholder="Ingrese la cantidad">
 	<button onClick="enviar()">Aceptar</button>
-	<br>
+	<div id="mostrar" style="margin: 15px;"></div>
 </body>
 </html>
 
 <script>
 	function enviar() {
-		// let lista1 = [];
-		// for (let index = 0; index < 8; index++) {
-		// 	let lista = [];
-		// 	for (let index = 0; index < 8; index++) {
-		// 		lista.push(0);			
-		// 	}
-		// 	lista1.push(lista);
-		// }
-		// console.log(lista1);
-
 		$.ajax({
 			type: "GET",
 			url: "pintar.php",
 			data: {
 				numero: $("#numero").val()
 			},
-			// dataType: "dataType",
+			beforeSend: function(){
+				$("#mostrar").empty();
+				$("#mostrar").append("<img src='cargando.gif' width='80px' id='imagen'> <br>Cargando...");
+			},
 			success: function (response) {
-				console.log(response);
+				// console.log(response);
+				$("#mostrar").empty();
+				$("#mostrar").append(response);
 			}
 		});
 	}
-
 </script>
 
 
